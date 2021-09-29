8875da00063645d226fc51d8edf0beda20c2d822
Niran Patel <72299269+githubNP1@users.noreply.github.com>
2021-09-29 14:55:20 +0100

    Update Segment.java

--- Segment.java
+++ Segment.java
@@ -17,18 +17,30 @@ public class Segment {
         this.direction = direction;
     }
     
+    public Segment(int x1, int y1, int x2, int y2, String direction, ArrayList<int[]> anchors){
+        for(int i = x1; i < x2; i++){
+            for(int j = y1; j < y2; j++){
+                int[] coord = {i, j};
+                pixels.add(coord);
+            }
+        }
+        this.direction = direction;
+        this.anchors = anchors;
+    }
+    
     public void adjust(int x, int y){
         for(int[] pixel : pixels){
             pixel[0] += x;
             pixel[1] += y;
         }
-        if(!anchors.isEmpty()){ System.out.println(1);
-            checkIfOnAnchor();
-        }
+        //if(!anchors.isEmpty()){ //System.out.println(1);
+        //    checkIfOnAnchor();
+        //}
     }
     
-    public void checkIfOnAnchor(){ //changes direction when at anchor - incomplete
-        if(pixels.get(0)[0] == anchors.get(0)[0] && pixels.get(0)[1] == anchors.get(0)[1]){
+    public void checkIfOnAnchor(){ 
+        //System.out.println(pixels.get(0)[0] + " " +  anchors.get(0)[0] + " " +  pixels.get(0)[1] + " " + anchors.get(0)[1]);
+        if(pixels.get(0)[0] == anchors.get(0)[0] && pixels.get(0)[1] == anchors.get(0)[1]){ //System.out.println(2);
             switch(anchors.get(0)[2]){
                 case 1:
                     direction = "up";
@@ -43,7 +55,7 @@ public class Segment {
                     direction = "left";
                 break;
             }
+            anchors.remove(0);
         }
-        anchors.remove(0);
     }
 }
